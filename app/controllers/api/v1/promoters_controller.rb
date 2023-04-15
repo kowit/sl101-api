b0cc42c7c7313fca50791c5252ac013f81b8a94b
Kowit karunas <kowitkarunas@gmail.com>
2023-04-15 01:39:09 -0400

    Added promoters actions.

---
+++ app/controllers/api/v1/promoters_controller.rb
@@ -0,0 +1,39 @@
+class Api::V1::PromotersController < ApplicationController
+  before_action :set_promoter, only: %i[ show update destroy ]
+
+  # GET /promoters
+  def index
+    @promoters = Promoter.all
+
+    render json: @promoters
+  end
+
+  # GET /promoters/1
+  def show
+    @promoter = Promoter.find(params[:id])
+
+    render json: @promoter
+  end
+
+  # POST /promoter
+  def create
+    @promoter = Promoter.new(promoter_params)
+
+    if @promoter.save
+      render json: @promoter, status: :created, location: @promoter
+    else
+      render json: @promoter.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_promoter
+      @promoter = Promoter.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def promoter_params
+      params.require(:promoter).permit(:email, :first_name, :last_name)
+    end
+end
