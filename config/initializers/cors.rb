8b0c91273f279e43b66555714f7723395174e677
Kowit karunas <kowitkarunas@gmail.com>
2023-05-25 00:27:21 -0400

    Updated cors.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:9091", "127.0.0.1:9091", "https://studentlife-nu.vercel.app"
+    origins "localhost:9091", "127.0.0.1:9091", "https://studentlife-mu.vercel.app"
 
     resource "*", headers: :any, methods: %i[get post patch put]
   end
